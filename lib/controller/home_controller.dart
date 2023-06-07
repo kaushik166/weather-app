d0320d6f22668bd03df794f0e043a519b00ec12c
kaushikkachhadiya <kachhadiya.kaushik110@gmail.com>
2023-06-07 16:20:13 +0530

    slash code

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -101,6 +101,7 @@ class HomeController extends GetxController {
       final connectivityResult = await Connectivity().checkConnectivity();
       if (connectivityResult != ConnectivityResult.none) {
         isLoading = true;
+        print("higihigigigiug");
         update();
 
         http.Response response = await http.get(
@@ -136,67 +137,3 @@ class HomeController extends GetxController {
     await useData(position.longitude, position.latitude);
   }
 }
-// String? currentAddress;
-// Position? _currentPosition;
-//
-// Future<bool> handleLocationPermission() async {
-//   bool serviceEnabled;
-//   LocationPermission permission;
-//
-//   serviceEnabled = await Geolocator.isLocationServiceEnabled();
-//   if (!serviceEnabled) {
-//     ScaffoldMessenger.of(BuildContext as BuildContext).showSnackBar(
-//         const SnackBar(
-//             content: Text(
-//                 'Location services are disabled. Please enable the services')));
-//     return false;
-//   }
-//   permission = await Geolocator.checkPermission();
-//   if (permission == LocationPermission.denied) {
-//     permission = await Geolocator.requestPermission();
-//     if (permission == LocationPermission.denied) {
-//       ScaffoldMessenger.of(BuildContext as BuildContext).showSnackBar(
-//           const SnackBar(content: Text('Location permissions are denied')));
-//       return false;
-//     }
-//   }
-//   if (permission == LocationPermission.deniedForever) {
-//     ScaffoldMessenger.of(BuildContext as BuildContext).showSnackBar(
-//         const SnackBar(
-//             content: Text(
-//                 'Location permissions are permanently denied, we cannot request permissions.')));
-//     return false;
-//   }
-//   return true;
-// }
-//
-// Future<void> getCurrentPosition() async {
-//   final hasPermission = await handleLocationPermission();
-//
-//   if (!hasPermission) return;
-//   await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
-//       .then((Position position) {
-//     _currentPosition = position;
-//     _getAddressFromLatLng(_currentPosition!);
-//   }).catchError((e) {
-//     debugPrint(e);
-//   });
-// }
-//
-// Future<void> _getAddressFromLatLng(Position position) async {
-//   await placemarkFromCoordinates(
-//           _currentPosition!.latitude, _currentPosition!.longitude)
-//       .then((List<Placemark> placemarks) {
-//     Placemark place = placemarks[0];
-//     currentAddress =
-//         '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
-//   }).catchError((e) {
-//     debugPrint(e);
-//   });
-// }
-//
-// @override
-// void onInit() {
-//   getCurrentPosition();
-//   super.onInit();
-// }
