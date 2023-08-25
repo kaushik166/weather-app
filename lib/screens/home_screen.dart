deb6f4b9e9db50c72b3ad11d13bd699614f28602
kaushikkachhadiya <kachhadiya.kaushik110@gmail.com>
2023-08-25 16:54:56 +0530

    slash code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -323,7 +323,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                                   controller
                                                       .weatherModel!.coord.lon
                                                       .toString()
-                                                      .substring(0, 5),
+                                                      .substring(0, 4),
                                                   style: const TextStyle(
                                                       fontSize: 22,
                                                       fontWeight:
